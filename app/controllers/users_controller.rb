ba8ac1246015731f78ea6186598b38f0cefaa5e4
akinori.tsuyama <ironika.pg.5@icloud.com>
2019-01-10 16:05:35 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: :show
+  
   def new
+    @user = User.new
   end
 
   def show
@@ -7,5 +10,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザを登録しました'
+      redirect_to @user
+    else
+      flash[:danger] = 'ユーザの登録に失敗しました'
+      render :new
+    end
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-                      uniquness: { case_sensitive: false }
+                      uniqueness: { case_sensitive: false }
     
     has_secure_password
 end
